56c729051cdacf743f074df7194d5608be2558b0
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2025-09-30 18:45:18 +0100

    add features

--- sas_projet/heads.h
+++ sas_projet/heads.h
@@ -1,6 +1,47 @@
 #ifndef H_HEADER
 #define H_HEADER
 
+#define  NOMBER_CATEGORIE 5
+#define NOMBER_PRODUIT 10
+#define SIZE_WORD 30
+#define EXIST_STOCK 5
+#define EMAIL_SIZE 100
+# define TEXT 200
+typedef struct 
+{
+   int id_categorie;
+   char nom_categorie[SIZE_WORD]
+} CATEGORIE;
+
+
+typedef struct 
+{
+    int d;
+    int m;
+    int y
+} date;
+struct Produit
+{
+  int  idProduit;
+  char nom[SIZE_WORD];
+  CATEGORIE categorie[NOMBER_CATEGORIE];
+  int  prix;
+  int  stock;
+  char description[TEXT]
+};
+
+struct Client 
+
+{
+   int  idClient ;
+   char nom[SIZE_WORD];
+   char pernom[SIZE_WORD];
+   char email[EMAIL_SIZE];
+   int solde
+};
+
+
+
 
 
 #endif
--- sas_projet/principale_sas.c
+++ sas_projet/principale_sas.c
@@ -1,12 +1,45 @@
-#include"heads.h"
+#include "heads.h"
 
+int main()
+{
 
+    int choix;
 
+    do
+    {
 
-int main(){
+        printf("1. cree compte si vous  plait ");
+        cre_compte();
+        prinf("vous pouvez afectuer choix de 1:modifier \n 2:consulter profile\n 3:Effectuer Achat\n 4:Mes Statistique\n 0: sortir");
+        printf("enter votre action sur profile");
+        scanf("%d", choix);
 
+        switch (choix)
+        {
+        case 1:
 
+            modifie();
+            break;
+        case 2:
+            consulter();
+            break;
+        case 3:
+            Effectuer_Achat();
+            break;
+        case 4:
+            Afficher_s();
+            break;
+        case 5:
+          
 
+        
+            break;
+        case 0:
+            printf("Thalla ");
+            return;
 
+        default:
+        }
 
+    } while (choix != 0)
 }
