fe9c1af0fae4f32f4ba675c550c318e771afdc1f
moussa <essalhimoussa64gmail.com>
2025-09-30 23:09:08 +0200

    structure of principale_sas file

--- sas_projet/principale_sas.c
+++ sas_projet/principale_sas.c
@@ -1,45 +1,109 @@
 #include "heads.h"
 
-int main()
-{
+int main() {
+    int choix, sousChoix;
+    Client client;
+    Produit produits[MAX_PRODUCTS] = {
+        {1, "Ordinateur Portable", "Electronique", 799.0, 5, "PC portable avec Intel i5, 8GB RAM"},
+        {2, "Smartphone", "Electronique", 499.0, 10, "Smartphone avec ecran 6 pouces"},
+        {3, "Casque Audio", "Accessoire", 89.0, 15, "Casque audio filaire avec micro"},
+        {4, "Souris", "Accessoire", 25.0, 20, "Souris optique USB"},
+        {5, "Clavier", "Accessoire", 45.0, 12, "Clavier mecanique USB"}
+    };
+    int nombre_produitProduits = 5;
+    char nomRecherche[MAX_NAME];
+    char catRecherche[MAX_NAME];
 
-    int choix;
+    printf("CREER VOTRE COMPTE\n");
+    client = cre_compte();
 
-    do
-    {
+    do {
+        printf("\n=== SYSTEME D'ACHAT CLIENT ===\n");
+        printf("1: Gestion profil\n");
+        printf("2: Gestion solde\n");
+        printf("3: Catalogue produits\n");
+        printf("4: Effectuer achat\n");
+        printf("5: Mes statistiques\n");
+        printf("0: Sortir\n");
+        printf("Entrer votre choix: ");
+        scanf("%d", &choix);
 
-        printf("1. cree compte si vous  plait ");
-        cre_compte();
-        prinf("vous pouvez afectuer choix de 1:modifier \n 2:consulter profile\n 3:Effectuer Achat\n 4:Mes Statistique\n 0: sortir");
-        printf("enter votre action sur profile");
-        scanf("%d", choix);
+        switch(choix) {
+            case 1:
+                printf("\n--- GESTION PROFIL ---\n");
+                printf("1: Modifier profil\n");
+                printf("2: Consulter profil\n");
+                printf("Entrer sous-choix: ");
+                scanf("%d", &sousChoix);
+                if(sousChoix == 1) client = modifie(client);
+                else if(sousChoix == 2) consulter(client);
+                else printf("Sous-choix invalide\n");
+                break;
 
-        switch (choix)
-        {
-        case 1:
+            case 2:
+                printf("\n--- GESTION SOLDE ---\n");
+                printf("1: Consulter solde\n");
+                printf("2: Deposer argent\n");
+                printf("Entrer sous-choix: ");
+                scanf("%d", &sousChoix);
+                if(sousChoix == 1) consulterSolde(client);
+                else if(sousChoix == 2) {
+                    float montant;
+                    printf("Entrer montant a deposer: ");
+                    scanf("%f", &montant);
+                    client = deposerSolde(client, montant);
+                } else printf("Sous-choix invalide\n");
+                break;
 
-            modifie();
-            break;
-        case 2:
-            consulter();
-            break;
-        case 3:
-            Effectuer_Achat();
-            break;
-        case 4:
-            Afficher_s();
-            break;
-        case 5:
-          
+            case 3:
+                printf("\n--- CATALOGUE PRODUITS ---\n");
+                printf("1: Afficher catalogue\n");
+                printf("2: Trier par prix\n");
+                printf("3: Trier par nom\n");
+                printf("4: Rechercher par nom\n");
+                printf("5: Rechercher par categorie\n");
+                printf("Entrer sous-choix: ");
+                scanf("%d", &sousChoix);
 
-        
-            break;
-        case 0:
-            printf("Thalla ");
-            return;
+                if(sousChoix == 1) afficherProduits(produits, nombre_produitProduits);
+                else if(sousChoix == 2) {
+                    trierProduitsParPrix(produits, nombre_produitProduits);
+                    afficherProduits(produits, nombre_produitProduits);
+                }
+                else if(sousChoix == 3) {
+                    trierProduitsParNom(produits, nombre_produitProduits);
+                    afficherProduits(produits, nombre_produitProduits);
+                }
+                else if(sousChoix == 4) {
+                    printf("Entrer nom produit: ");
+                    scanf("%s", nomRecherche);
+                    rechercherProduitParNom(produits, nombre_produitProduits, nomRecherche);
+                }
+                else if(sousChoix == 5) {
+                    printf("Entrer categorie: ");
+                    scanf("%s", catRecherche);
+                    rechercherProduitParCategorie(produits, nombre_produitProduits, catRecherche);
+                }
+                else printf("Sous-choix invalide\n");
+                break;
 
-        default:
+            case 4:
+                client = Effectuer_Achat(client, produits, nombre_produitProduits);
+                break;
+
+            case 5:
+                Afficher_s(client);
+                break;
+
+            case 0:
+                printf("Au revoir\n");
+                break;
+
+            default:
+                printf("Choix invalide\n");
         }
 
-    } while (choix != 0)
+    } while(choix != 0);
+
+    return 0;
 }
